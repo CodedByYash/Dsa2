f693018193c063cf5efc7e7662be062d1bff3646
Yash83085 <gawadeyash0@gmail.com>
2025-01-09 20:33:59 +0530

    solved 2 question of 8 Jan

---
+++ 09-01-2025/decode_ways.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+            return 0; // No valid decoding for empty string or starting with '0'
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        // Base cases
+        dp[0] = 1; // Empty string has one valid decoding
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+
+        // Dynamic programming to fill the dp array
+        for (int i = 2; i <= n; i++) {
+            // Single digit decoding
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // Double digit decoding
+            int twoDigit = Integer.parseInt(s.substring(i - 2, i));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
---
+++ 09-01-2025/word_break.java
@@ -0,0 +1,52 @@
+class Solution {
+    class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean isWord = false;
+    }
+
+    TrieNode root = new TrieNode();
+
+    // Function to insert a word into the Trie
+    private void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            node.children.putIfAbsent(c, new TrieNode());
+            node = node.children.get(c);
+        }
+        node.isWord = true;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // Build Trie with words from the wordDict
+        for (String word : wordDict) {
+            insert(word);
+        }
+
+        // Use a memoization map to avoid redundant calculations
+        Map<Integer, Boolean> memo = new HashMap<>();
+        return dfs(s, 0, memo);
+    }
+
+    private boolean dfs(String s, int index, Map<Integer, Boolean> memo) {
+        if (index == s.length()) return true; // Successfully segmented
+        if (memo.containsKey(index)) return memo.get(index); // Return cached result
+
+        TrieNode node = root;
+        for (int i = index; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!node.children.containsKey(c)) break;
+
+            node = node.children.get(c);
+            if (node.isWord) {
+                // If we find a valid word, recursively call dfs for the next part of the string
+                if (dfs(s, i + 1, memo)) {
+                    memo.put(index, true);
+                    return true;
+                }
+            }
+        }
+
+        memo.put(index, false); // No valid segmentation found
+        return false;
+    }
+}
