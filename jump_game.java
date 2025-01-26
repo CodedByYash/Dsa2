82da57374c7b8f7216c8b27900850ac033b67b20
👑Ash👑 <113412567+CodedByYash@users.noreply.github.com>
2025-01-26 22:51:10 +0530

    solved 1 question of Jan 26

---
+++ jump_game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0; // Keeps track of the maximum index reachable
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false; // If we can't reach the current index, return false
+            }
+            maxReach = Math.max(maxReach, i + nums[i]); // Update the maximum reachable index
+        }
+        return true; // If we finish the loop, it means we can reach the last index
+    }
+}
