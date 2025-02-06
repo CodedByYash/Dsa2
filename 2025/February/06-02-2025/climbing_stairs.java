c4cf163d3745831b09a64a48e5de8a52cd4dc2a4
CodedByYash <gawadeyash0@gmail.com>
2025-02-06 23:45:48 +0530

    solved 1 question of feb 6

---
+++ 2025/February/06-02-2025/climbing_stairs.java
@@ -0,0 +1,14 @@
+public class climbing_stairs {
+    public int climbStairs(int n) {
+        if (n <= 2)
+            return n; // Base cases
+
+        int prev1 = 1, prev2 = 2;
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+}
