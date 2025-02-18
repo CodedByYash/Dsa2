9ac9bdc1c13806b2fb1d7a52e8af8e1e544ff7be
CodedByYash <gawadeyash0@gmail.com>
2025-02-18 20:10:26 +0530

    solved 1 question of Feb 18

---
+++ 2025/February/18-02-2025/gray-code.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class gray-code
+{
+
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+        
+        for (int i = 0; i < n; i++) {
+            int addValue = 1 << i;
+            int size = result.size();
+            
+            for (int j = size - 1; j >= 0; j--) {
+                result.add(result.get(j) + addValue);
+            }
+        }
+        
+        return result;
+    }
+}
